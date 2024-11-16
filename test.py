d888747e2b8eb5d0bbac163710ec2de87be255de
DESKTOP-8BL3MIG <haseebahamadnoul@gmail.com>
2024-11-16 13:29:26 +0500

    Add up part of UNet(upgrading)

--- test.py
+++ test.py
@@ -26,6 +26,19 @@ class UNET(nn.modules):
         self.pool = nn.MaxPool2d(kernel_size = 2, stride = 2)
         
         # down part of UNet
-        for featue in features():
+        for feature in features:
             self.downs.append(DoubleConv(in_channels, features))
             in_channels = features
+
+        #up part of UNet
+        for feature in reversed(features):
+            self.ups.append(
+                nn.ConvTranspose2d(
+                    features*2, feature, kernel_size = 2, stride = 2
+                )
+            )
+            self.ups.append(DoubleConv(features*2, feature))
+
+        self.bottleneck = DoubleConv(features[-1], features[-1]*2)
+        self.final_conv = nn.Conv2d(features[0], out_channels, kernel_size = 1)
+     
