6eef209ac5ed8ca79b6ec6882ff1a2e8d5955ed5
DESKTOP-8BL3MIG <haseebahamadnoul@gmail.com>
2024-11-18 11:49:45 +0500

    setup a training fun which helps in training the model

--- training.py
+++ training.py
@@ -18,15 +18,24 @@ from utils import(
 # Hyperparameters etc.
 LEARNING_RATE = 1e-4
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-BATCH_SIZE = 32
-NUM_EPOCHS = 100
+BATCH_SIZE = 16
+NUM_EPOCHS = 3
 NUM_WORKERS = 2
-IMAGE_HEIGHT = 160
-IMAGE_WIDTH = 240
+IMAGE_HEIGHT = 160 #1280 originally
+IMAGE_WIDTH = 240 #1918 originally
 PIN_MEMORY = True
-LOAD_MODEL = True
+LOAD_MODEL = False
 TRAIN_IMG_DIR = "data/train_images/"
 TRAIN_MASK_DIR = "data/train_masks/"
 VAL_IMG_DIR = "data/val_images/"
 VAL_MASK_DIR = "data/val_masks/"
 
+#def to train model with these parameters
+def train_fn (loader, model, optimizer, loss_fn, scalar):
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
